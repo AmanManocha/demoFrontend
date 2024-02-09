cc52527ca3d8403f328ad7fa3cdd12590e922cb8
AmanManocha <aman.manocha@netsmartz.net>
2024-02-09 14:55:35 +0530

    search

--- src/pages/user.js
+++ src/pages/user.js
@@ -1,138 +1,30 @@
-// import React, { useState, useEffect } from 'react';
-// import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-// import { DataGrid } from '@mui/x-data-grid';
-// import axios from 'axios';
-
-// const TablePage = () => {
-//   const [data, setData] = useState([]);
-
-//   const token = localStorage.getItem('accessToken');
-
-//   const columns = [
-//     { field: 'id', headerName: 'S no.', width: 70 },
-//     { field: 'firstName', headerName: 'First name', width: 130 },
-//     { field: 'lastName', headerName: 'Last name', width: 130 },
-//     {
-//       field: 'age',
-//       headerName: 'Age',
-//       type: 'number',
-//       width: 90,
-//     },
-//     {
-//       field: 'fullName',
-//       headerName: 'Full name',
-//       description: 'This column has a value getter and is not sortable.',
-//       sortable: false,
-//       width: 160,
-//       valueGetter: (params) =>
-//         `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-//     },
-//   ];
-//   const rows = [
-//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-//   ];
-
-//   useEffect(() => {
-//     // Replace 'https://api.example.com/data' with the actual API endpoint
-//     axios.get('http://127.0.0.1:3000/api/auth/userData', {
-//       headers: {
-//         accessToken: token
-//       }
-//     })
-//       .then(response => {
-//         setData(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching data:', error);
-//       });
-//   }, [token]); // Include 'token' in the dependency array to trigger a re-fetch when the token changes
-
-
-//   return (
-//     <div style={{ height: 400, width: '100%' }}>
-//       <DataGrid
-//         rows={rows}
-//         columns={columns}
-//         initialState={{
-//           pagination: {
-//             paginationModel: { page: 0, pageSize: 5 },
-//           },
-//         }}
-//         pageSizeOptions={[5, 10]}
-//       />
-//     </div>
-//   );
-// //   return (
-// //     <Paper>
-// //       <TableContainer>
-// //         <Table>
-// //           <TableHead>
-// //             <TableRow>
-// //               <TableCell>S NO.</TableCell>
-// //               <TableCell>First Name</TableCell>
-// //               <TableCell>Last Name</TableCell>
-// //               <TableCell>City</TableCell> {/* Assuming 'city' is a property in your data */}
-// //             </TableRow>
-// //           </TableHead>
-// //           <TableBody>
-// //             {data.map((row, index) => (
-// //               <TableRow key={index}>
-// //                 <TableCell>{index + 1}</TableCell>
-// //                 <TableCell>{row.firstName}</TableCell>
-// //                 <TableCell>{row.lastName}</TableCell>
-// //                 <TableCell>{row.city}</TableCell>
-// //               </TableRow>
-// //             ))}
-// //           </TableBody>
-// //         </Table>
-// //       </TableContainer>
-// //     </Paper>
-// //   );
-// };
-
-// export default TablePage;
-
-
 import React, { useState, useEffect } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Paper, TablePagination } from '@mui/material';
 import axios from 'axios';
-
+import './table.css'
 const TablePage = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
   const token = localStorage.getItem('accessToken');
 
   const columns = [
-    { field: 'id', headerName: 'S no.', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-    {
-        field: 'email',
-        headerName: 'Email',
-        width: 350,
-      },
+    // { id: 'id', label: 'S no.', minWidth: 70 },
+    // { id: 'firstName', label: 'First name', minWidth: 130 },
+    // { id: 'lastName', label: 'Last name', minWidth: 130 },
+    { id: 'fullName', label: 'Full name'},
+    { id: 'age', label: 'Age'},
+    { id: 'email', label: 'Email' },
+    { id: 'city', label: 'City'},
+    { id: 'pinCode', label: 'Pin Code'},
+    { id: 'location', label: 'Location'},
+    { id: 'profession', label: 'Profession'},
+    { id: 'project', label: 'Project'},
+    { id: 'experience', label: 'Experience'},
+    { id: 'projectDuration', label: 'Project Duration'},
+    { id: 'profilePhoto', label: 'Profile Photo'},
+    { id: 'documents', label: 'Documnets'},
   ];
 
   useEffect(() => {
@@ -143,23 +35,89 @@ const TablePage = () => {
     })
       .then(response => {
         // Add a unique identifier to each row
-        const rowsWithId = response.data.map((row, index) => ({ ...row, id: index }));
+        const rowsWithId = response.data.map((row, index) => ({ ...row, id: index + 1 }));
         setData(rowsWithId);
+        console.log(rowsWithId ,"ggggggggggggggggggggg")
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
   }, [token]);
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredData = data.filter(row => {
+    const { firstName, lastName, age, email } = row;
+    const lowerCaseQuery = searchQuery.toLowerCase();
+    return (
+      firstName && firstName.toLowerCase().includes(lowerCaseQuery) ||
+      lastName && lastName.toLowerCase().includes(lowerCaseQuery) ||
+      age && age.toString().includes(lowerCaseQuery) ||
+      email && email.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={data}
-        columns={columns}
-        pageSize={5}
-        pagination
+    <Paper>
+        <div className="searchContainer">
+      <TextField  className="searchBar"
+        label="Search"
+        variant="outlined"
+        value={searchQuery}
+        onChange={handleSearchChange}
+        margin="normal"
       />
-    </div>
+      </div>
+      <TableContainer className="tableContainer">
+        <Table className="table">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell key={column.id} style={{ border: '1px solid #e0e0e0' }}>
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody >
+            {filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => (
+              <TableRow key={row.id}>
+                {columns.map(column => (
+                  <TableCell key={column.id} align={column.align} style={{ border: '1px solid #e0e0e0' }}>
+                     {column.id === 'fullName' ? `${row.firstName} ${row.lastName}` :
+                     column.id === 'location' ? `lat: ${row.location.lat} - lng: ${row.location.lng} ` :
+                     column.id === 'projectDuration' ? `Start Date: ${row.projectDuration.startDate} - End Date: ${row.projectDuration.endDate}` :
+                     column.id === 'documents' ? row.documents.join(' , ') :
+                    row[column.id]}
+                    {/* {row[column.id]} */}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination className="pagination"
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={filteredData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
 
